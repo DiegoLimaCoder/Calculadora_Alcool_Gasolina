1381054f7d24d37dec532e1bc072cf48b7e6daf1
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2024-07-29 13:02:41 -0300

    Implementando a lógica

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,88 @@
+import { FormEvent, useState } from "react";
 import "./App.css";
 
 import logoImg from "./assets/logo.png";
 
+interface InfoProps {
+  title: string;
+  gasolina: string | number;
+  alcool: string | number;
+}
+
 function App() {
+  const [gasolinaInput, setGasolinaInput] = useState(0);
+  const [alcoolInput, setAlcoolInput] = useState(0);
+  const [info, setInfo] = useState<InfoProps>();
+
+  function calcular(event: FormEvent) {
+    event.preventDefault();
+
+    const calculo = alcoolInput / gasolinaInput;
+
+    if (calculo <= 0.7) {
+      setInfo({
+        title: "Compensa usar Álcool",
+        gasolina: formatarMoeda(gasolinaInput),
+        alcool: formatarMoeda(alcoolInput),
+      });
+    } else {
+      setInfo({
+        title: "Compensa usar gasolina",
+        gasolina: formatarMoeda(gasolinaInput),
+        alcool: formatarMoeda(alcoolInput),
+      });
+    }
+  }
+
+  function formatarMoeda(valor: number) {
+    const valorFormatado = valor.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+
+    return valorFormatado;
+  }
+
   return (
     <>
-      <div className="container">
-        <img className="logo" src={logoImg} alt="logo da gasolina x Álcool" />
-        <h1 className="title">Qual é a melhor opção ?</h1>
-        <form className="form">
-          <label>Álcool (Preço por litro):</label>
-          <input
-            type="number"
-            placeholder="4,90"
-            className="input"
-            min="1"
-            step="0.1"
-            required
-          />
-          <label>Gasolina (Preço por litro):</label>
-          <input
-            type="number"
-            placeholder="4,90"
-            className="input"
-            min="1"
-            step="0.1"
-            required
-          />
-          <input className="button" type="submit" value="Calcular" />
-        </form>
+      <div>
+        <main className="container">
+          <img className="logo" src={logoImg} alt="logo da gasolina x Álcool" />
+          <h1 className="title">Qual é a melhor opção ?</h1>
+          <form className="form" onSubmit={calcular}>
+            <label>Álcool (Preço por litro):</label>
+            <input
+              type="number"
+              placeholder="4,90"
+              className="input"
+              min="1"
+              step="0.1"
+              value={alcoolInput}
+              onChange={(e) => setAlcoolInput(Number(e.target.value))}
+              required
+            />
+            <label>Gasolina (Preço por litro):</label>
+            <input
+              type="number"
+              placeholder="4,90"
+              className="input"
+              min="1"
+              step="0.1"
+              required
+              value={gasolinaInput}
+              onChange={(e) => setGasolinaInput(Number(e.target.value))}
+            />
+            <input className="button" type="submit" value="Calcular" />
+          </form>
+
+          {info && Object.keys(info).length > 0 && (
+            <section className="result">
+              <h2 className="result-title">{info.title}</h2>
+              <span>Álcool {info.alcool}</span>
+              <span>Gasolina {info.gasolina}</span>
+            </section>
+          )}
+        </main>
       </div>
     </>
   );
